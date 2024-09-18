b4efc1bd98935af76b09c850b6a9d2453777e235
Pablo Barbero <pablobarbero220@gmail.com>
2024-09-18 02:00:25 -0300

    fix: gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://barberopablo.github.io/medTrace/",
+  base: "https://barberopablo.github.io/MedTrace/",
   plugins: [react()],
 });
